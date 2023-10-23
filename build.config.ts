b6e25ad819f0d7f78b76502e522bb9ab74be21e1
kazuya kawaguchi <kawakazu80@gmail.com>
2023-10-23 18:19:46 +0900

    initial setup

---
+++ build.config.ts
@@ -0,0 +1,10 @@
+import { defineBuildConfig } from 'unbuild'
+
+export default defineBuildConfig({
+  declaration: true,
+  rollup: {
+    emitCJS: true,
+  },
+  entries: ['./src/index.ts'],
+  externals: ['hono'],
+})
